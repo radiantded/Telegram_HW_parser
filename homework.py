f7fcb3f158e06fe91c9ba97046ab25ba8163999d
Enver <radiantded@gmail.com>
2021-04-28 18:42:51 +0300

    ok

--- homework.py
+++ homework.py
@@ -1,8 +1,10 @@
+import logging
 import os
 import time
 
 import requests
 import telegram
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,23 +16,27 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+    if homework['status'] == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+    elif homework['status'] == 'approved':
+        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'  # noqa
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    date = {'from_date': current_timestamp}
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    homework_statuses = requests.get(
+        'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
+        params=date, headers=headers
+    )
     return homework_statuses.json()
 
 
 def send_message(message, bot_client):
-    ...
-    return bot_client.send_message(...)
+    bot_client = telegram.Bot(TELEGRAM_TOKEN)
+    return bot_client.send_message(chat_id=CHAT_ID, text='message')
 
 
 def main():
@@ -41,14 +47,24 @@ def main():
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
+                send_message(
+                    parse_homework_status(new_homework.get('homeworks')[0])
+                )
+            current_timestamp = new_homework.get(
+                'current_date', current_timestamp
+            )  # обновить timestamp
             time.sleep(300)  # опрашивать раз в пять минут
 
-        except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
+        except Exception as error:
+            print(f'Бот столкнулся с ошибкой: {error}')
             time.sleep(5)
 
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename='main.log',
+    format='%(asctime)s, %(levelname)s, %(name)s, %(message)s'
+)
 
 if __name__ == '__main__':
     main()
