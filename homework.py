db1df92eec90477ea771980170b1462b10d3813c
Enver <radiantded@gmail.com>
2021-04-29 18:06:33 +0300

    ok

--- homework.py
+++ homework.py
@@ -13,52 +13,7 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-
-
-def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    if homework['status'] == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    elif homework['status'] == 'approved':
-        verdict = (f'{"Ревьюеру всё понравилось, "}'
-                   f'{"можно приступать к следующему уроку."}')
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
-
-
-def get_homework_statuses(current_timestamp):
-    date = {'from_date': current_timestamp}
-    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
-    homework_statuses = requests.get(
-        'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
-        params=date, headers=headers
-    )
-    return homework_statuses.json()
-
-
-def send_message(message, bot_client):
-    bot_client = telegram.Bot(TELEGRAM_TOKEN)
-    return bot_client.send_message(chat_id=CHAT_ID, text='message')
-
-
-def main():
-    # проинициализировать бота здесь
-    current_timestamp = int(time.time())  # начальное значение timestamp
-
-    while True:
-        try:
-            new_homework = get_homework_statuses(current_timestamp)
-            if new_homework.get('homeworks'):
-                send_message(
-                    parse_homework_status(new_homework.get('homeworks')[0])
-                )
-            current_timestamp = new_homework.get(
-                'current_date', current_timestamp
-            )  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
-
-        except Exception as error:
-            print(f'Бот столкнулся с ошибкой: {error}')
-            time.sleep(5)
+LOGGER = logging.getLogger(__name__)
 
 
 logging.basicConfig(
@@ -66,6 +21,64 @@ logging.basicConfig(
     filename='main.log',
     format='%(asctime)s, %(levelname)s, %(name)s, %(message)s'
 )
+
+
+def parse_homework_status(homework):
+    homework_name = homework.get('homework_name')
+    hw_status = homework.get('status')
+    if homework_name is not None:
+        if hw_status is not None:
+            statuses = {
+               'rejected': 'К сожалению в работе нашлись ошибки.',
+               'approved': ('Ревьюеру всё понравилось, '
+                            'можно приступать к следующему уроку.')
+            }
+            verdict = statuses.get(hw_status)
+            return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    return 'Нет данных'
+
+
+def get_homework_statuses(current_timestamp):
+    timestamp = (current_timestamp if current_timestamp
+                 is not None else int(time.time()))
+    date = {'from_date': timestamp}
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    try:
+        homework_statuses = requests.get(
+            'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
+            params=date, headers=headers
+        )
+        return homework_statuses.json()
+    except Exception:
+        LOGGER.exception()
+
+
+def send_message(message, bot_client):
+    return bot_client.send_message(chat_id=CHAT_ID, text='message')
+
+
+def main():
+    bot_client = telegram.Bot(TELEGRAM_TOKEN)
+    current_timestamp = int(time.time())  # начальное значение timestamp
+
+    while True:
+        try:
+            new_homework = get_homework_statuses(current_timestamp)
+            if new_homework.get('homeworks'):
+                send_message(
+                    parse_homework_status(new_homework.get('homeworks')[0]),
+                    bot_client
+                )
+            current_timestamp = new_homework.get(
+                'current_date', current_timestamp
+            )  # обновить timestamp
+            time.sleep(300)  # опрашивать раз в пять минут
+
+        except Exception:
+            logging.exception()
+            send_message('Произошел сбой в работе бота', bot_client)
+            time.sleep(5)
+
 
 if __name__ == '__main__':
     main()
